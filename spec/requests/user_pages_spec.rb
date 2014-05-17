543ded783fd928bf2f79ee8deca4e34c87ea5cbc
Tony Ta <tonyta.tt@gmail.com>
2014-05-16 23:59:13 -0700

    adds extra refactoring from ch 5 excercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,17 +26,37 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_selector("div.alert.alert-error", text: 'error') }
+      end
+
       it "should not create user" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
 
     describe "with valid information" do
+      let(:user) do
+        double(:user, name:     "Example User",
+                      email:    "user@example.com",
+                      password: "foobar")
+      end
+
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Name",         with: user.name
+        fill_in "Email",        with: user.email
+        fill_in "Password",     with: user.password
+        fill_in "Confirmation", with: user.password
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
       it "should create a user" do
